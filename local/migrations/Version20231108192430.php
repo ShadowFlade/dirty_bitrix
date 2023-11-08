57ca988e9c187f007f4f0490b39eb0d77aa13674
Vladimir Drugov <wolffdrugov@yandex.ru>
2023-11-08 19:32:29 +0300

    master:added locl + migrations

---
+++ local/migrations/Version20231108192430.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20231108192430 extends Version
+{
+    protected $description = "ИБ Новости - создание свойства Сложность";
+
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('news', 'news');
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Сложность',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'DIFFICULTY',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '1',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+));
+    
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
